1145b17bfb9a63518620e350bb08b1b5987f90fe
DaproDX <155269761+DAPRO2012@users.noreply.github.com>
2026-09-02 00:14:41 +0100

    Add internet request handling in internet.lua
    Implement internet request handling with dependency management and script execution.

---
+++ internet.lua
@@ -0,0 +1,78 @@
+local modems = { peripheral.find("modem", function(name, modem)
+    return modem.isWireless()
+end) }
+
+if #modems == 0 then
+    printError("No interdimensional modem attached!")
+    return
+end
+
+function defaultHandler(response)
+    if response.Headers and response.Headers.Type == "script" then
+        if response.Headers.Dependencies ~= nil then
+            print("This script requires dependencies:")
+            term.setTextColour(colors.lightBlue)
+            
+            for name, uri in pairs(response.Headers.Dependencies) do
+                print("    " .. name .. " | " .. uri)
+            end
+            term.setTextColour(colors.white)
+
+            write("Would you like to proceed? (y/n): ")
+            local proceed = string.lower(read())
+            if proceed ~= "y" then return nil end
+        
+            for name, uri in pairs(response.Headers.Dependencies) do
+                shell.run(uri)
+            end
+        end
+        
+        if not fs.exists("networking") then
+            fs.makeDir("networking")
+        end
+            
+        if not fs.exists("networking/scripts/") then
+            fs.makeDir("networking/scripts")
+        end
+            
+        local file = fs.open("networking/scripts/" .. response.Headers.name, "w")
+        file.write(response.Body)
+        file.close()
+        shell.run("networking/scripts/" .. response.Headers.name)
+
+        return nil
+    end
+    
+    print(response.Body)
+end
+
+_G.internetHandler = defaultHandler
+
+function Request(Channel, URI, Method, Headers, Body)
+    local myChannel = os.getComputerID()
+    
+    local packet = {
+        uri = URI,
+        method = Method,
+        Headers = Headers,
+        Body = Body
+    }
+    
+    modems[1].open(myChannel)
+    modems[1].transmit(Channel, myChannel, packet)
+    
+    local event, side, rChannel, rReplyChannel, message, distance
+    repeat
+        event, side, rChannel, rReplyChannel, message, distance = os.pullEvent("modem_message")
+    until rChannel == myChannel
+    
+    _G.internetHandler(message)
+end
+
+local mockPacket = {}
+mockPacket.Body = "print('Hello World!')"
+mockPacket.Headers = {}
+mockPacket.Headers.Type = "script"
+mockPacket.Headers.name = "test"
+
+_G.internetHandler(mockPacket)
