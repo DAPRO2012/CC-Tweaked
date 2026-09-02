76f6c765da133a5f3ea3865619a1e12b82a630c6
DaproDX <155269761+DAPRO2012@users.noreply.github.com>
2026-09-02 16:12:19 +0100

    Refactor internet request to use internetHandler

--- search.lua
+++ search.lua
@@ -4,5 +4,4 @@ local internet = require("internet")
 if #args < 2 then
   error("Usage: <channel> <uri>")
 end
-
-internet.request(tonumber(args[1]), args[2], "GET", {}, "")
+internetHandler(internet.request(tonumber(args[1]), args[2], "GET", {}, ""))
