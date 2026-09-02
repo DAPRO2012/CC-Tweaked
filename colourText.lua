c83c64fc98bbd11ce6e911900aa9ec750ee08048
DaproDX <155269761+DAPRO2012@users.noreply.github.com>
2026-09-02 17:24:18 +0100

    Add printColored function for colored text output

---
+++ colourText.lua
@@ -0,0 +1,37 @@
+local function printColored(textString, defaultBg)
+    local textOut = ""
+    local colorOut = ""
+    local bgOut = ""
+    
+    local currentColor = "0"
+    local currentBg = defaultBg or "f"
+    
+    local i = 1
+    while i <= #textString do
+        local char = textString:sub(i, i)
+       
+        if char == "&" and i < #textString do
+            local nextChar = textString:sub(i + 1, i + 1)
+            if nextChar:match("[0-9a-fA-F]") then
+                currentColor = nextChar:lower()
+                i = i + 2
+            else
+                textOut = textOut .. char
+                colorOut = colorOut .. currentColor
+                bgOut = bgOut .. currentBg
+                i = i + 1
+            end
+        else
+            textOut = textOut .. char
+            colorOut = colorOut .. currentColor
+            bgOut = bgOut .. currentBg
+            i = i + 1
+        end
+    end
+    
+    if #textOut > 0 then
+        term.blit(textOut, colorOut, bgOut)
+        local x, y = term.getCursorPos()
+        term.setCursorPos(1, y + 1)
+    end
+end
